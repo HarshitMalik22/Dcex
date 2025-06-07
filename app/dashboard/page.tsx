0fb2c3500cfa5b29c01f8b3911af4bc4bdbaf5cc
Harshit <harshitmalik29@gmail.com>
2025-06-08 02:06:50 +0530

    greeting

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 import {useSession} from "next-auth/react"
+import { useRouter } from "next/navigation";
 export default function Dashboard() {
     const session = useSession();
+    const router = useRouter();
+
+    if (session.status === "loading"){
+        return <div>
+            Loading...
+        </div>
+    }
+
+    if (!session.data?.user){
+        router.push("/")
+    }
     return <div className="pt-8 flex justify-center">
-        <div className="max-w-xl bg-white rounded shadow">
-            hi there
+        <div className="max-w-4xl bg-white rounded shadow w-full p-12">
+            <Greeting 
+            image={session.data?.user?.image ?? ""} 
+            name={session.data?.user?.name ?? ""} 
+            />
         </div>
         
     </div>
+}
+function Greeting({image, name}: {image: string, name: string})
+{
+    return <div className="flex">
+        <img src={image} className="rounded-full w-16 h-16 mr-4"/>
+        <div className="text-2xl font-bold ml-4 flex flex-col justify-center"/>
+            Welcome Back, {name}
+    </div>
+
 }
