09a97344a42b035e2c3ad7061706581f5a4d1162
Harshit <harshitmalik29@gmail.com>
2025-06-08 21:25:17 +0530

    installations

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,10 @@
 import { Connection } from "@solana/web3.js";
 
-export const SUPPORTED_TOKENS: {
+let LAST_UPDATED = null;
+let prices = {};
+const TOKEN_PRICE_REFRESH_INTERVAL = 60 * 1000;
+
+export let SUPPORTED_TOKENS: {
     name: string;
     mint: string;
 }  = [{
@@ -14,15 +18,22 @@ export const SUPPORTED_TOKENS: {
     name: "USDT",
     mint: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
     symbol: "USDT",
-    decimals: 6,
-    icon: "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
 },
 {
     name: "SOL",
     mint: "So11111111111111111111111111111111111111112",
-    symbol: "SOL",
-    decimals: 9,
-    icon: "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
+   
 }]
 
 export const connection = new Connection("https://api.mainnet-beta.solana.com");
+
+export async function getSupportedTokens(){
+    if (!LAST_UPDATED && new Date().getTime() - LAST_UPDATED < TOKEN_PRICE_REFRESH_INTERVAL){
+        const response = await axios.get(:"https://price.jup.ag/v6/price?ids=SOL,USDC,USDT");
+        prices = response.data.data;
+        LAST_UPDATED = new Date().getTime();
+    }
+    SUPPORTED_TOKENS.map(s => )
+}
+
+getSupportedTokens();
